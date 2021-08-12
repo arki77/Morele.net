126335280499361b157e3a4bd953d55c300f6069
arki <biznes.arki@gmail.com>
2021-08-12 14:11:50 +0200

    dpdppdpd

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from bs4 import BeautifulSoup
 
 
 def read_token():
-	with open(r"data\token.txt", "r") as f:
+	with open("token.txt", "r") as f:
 		lines = f.readlines()
 		return lines[0].strip()
 
@@ -43,7 +43,7 @@ client.remove_command('help')
 
 
 
-dataBase = r"data\database.json"
+dataBase = "data\database.json"
 
 
 
